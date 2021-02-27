61152fcdbf23a77f2b3e94b423f6cbe07338b904
Pilar Aparecida <pilarinfodsgn@gmail.com>
2021-02-27 14:25:05 -0300

    Delete ed-4/nodejs_api-nps directory

--- ed-4/nodejs_api-nps/src/errors/app-error.ts
+++
@@ -1,6 +0,0 @@
-export class AppError {
-    constructor(public readonly message: string, public readonly statusCode: number = 400) {
-        this.message = message;
-        this.statusCode = statusCode;
-    }
-}
--- ed-4/nodejs_api-nps/src/repositories/surveys-user-repository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { SurveyUser } from "../models/surveys-user";
-
-@EntityRepository(SurveyUser)
-class SurveysUsersRepository extends Repository<SurveyUser>{ }
-
-export { SurveysUsersRepository }
